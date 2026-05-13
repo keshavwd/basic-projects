9ba3a3cb1c0ba26d3ec01b936ac24f4e370bb061
keshavwd <keshav.wd@gmail.com>
2026-05-13 11:24:55 +0530

    Refactor tic-tac-toe game with improved logic
    Refactored tic-tac-toe game logic and improved user interface.

--- tic-tak-toe.py
+++ tic-tak-toe.py
@@ -1,24 +1,46 @@
-ini_val = [1,2,3,4,5,6,7,8,9]
+b_list = [1,2,3,4,5,6,7,8,9]
 player = 'X'
 win = [(0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6)]
-while True:
-    print("\n\t\t\t TIC-TAC-TOE")
-    print(f"\n\t\t\t {ini_val[0]} | {ini_val[1]} | {ini_val[2]}")
-    print("\t\t\t ---------")
-    print(f"\t\t\t {ini_val[3]} | {ini_val[4]} | {ini_val[5]}")
-    print("\t\t\t ---------")
-    print(f"\t\t\t {ini_val[6]} | {ini_val[7]} | {ini_val[8]}")
+flag = 0
+count = 0
 
-    print(f"\n\t\t\tPlayer {player} turn : ", end="")
-    turn = int(input())
-    ini_val[turn-1] = player
+while True:
+    print("\nTIK TAC TOE")
+    #playground grid
+    print('\n-------------')
+    print(f'  {b_list[0]} | {b_list[1]} | {b_list[2]}  ')
+    print('-------------')
+    print(f'  {b_list[3]} | {b_list[4]} | {b_list[5]}  ')
+    print('-------------')
+    print(f'  {b_list[6]} | {b_list[7]} | {b_list[8]}  ')
+    print('-------------')
+    print('\n')
+
+    #exit game if player wins
+    if flag == 1:
+        break
+    if count == 9:
+        print("Match draw")
+        break
+    
     for a,b,c in win:
-        if ini_val[a] == ini_val[b] and ini_val[b] == ini_val[a]:
-            print(ini_val[a] == ini_val[b] and ini_val[b] == ini_val[a])
-            print(f"Player {player} wins.")
-    if player == 'X':
-        player = 'O'
-        #ini_val[turn] = 'X'
+        if b_list[a] == b_list[b] and b_list[b] == b_list[c]:
+            print(f"Player {player} wins! Congratulations.")
+            flag = 1;
+         
+    #player input
+    print(f"Player {player} turn : ", end="")
+    playerInput = int(input())
+
+    if playerInput in b_list:        
+        #grid player entry
+        b_list[playerInput-1] = player
+        #counter
+        count = count+1
+        #player switch
+        if player == 'X':
+            player = 'O'
+        else:
+            player = 'X'
     else:
-        player = 'X'
-        #ini_val[turn] = 'O'
+        print("Please enter the correct value.")
